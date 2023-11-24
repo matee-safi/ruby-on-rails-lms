6097605912807667b698e554f40eb3e71bdb387a
Matee <matiullahsafibbc@gmail.com>
2023-11-24 18:10:51 +0430

    install flowbite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,7 @@ class UsersController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = user.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :grades
   resources :courses
   resources :academic_classes
+  resources :users
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
